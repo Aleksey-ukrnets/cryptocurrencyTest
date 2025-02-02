ddda3a26ac5205a83ee79cfc7ca3ab2cd24ea64a
Alexey Ukrainets <aleksey.ukrnets@mail.ru>
2025-02-02 15:31:25 +0300

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'components': path.resolve(__dirname, 'src/components'),
-      'shared': path.resolve(__dirname, 'src/shared'),
+      components: path.resolve(__dirname, "src/components"),
+      shared: path.resolve(__dirname, "src/shared"),
     },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'https://pro-api.coinmarketcap.com',
+      "/api": {
+        target: "https://pro-api.coinmarketcap.com",
         changeOrigin: true,
         secure: false,
-        rewrite: (p: string) => p.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+        rewrite: (p: string) => p.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
