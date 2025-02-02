6c71e22333787aa0a2051de660b6712cf8b42fba
Alexey Ukrainets <aleksey.ukrnets@mail.ru>
2025-02-02 15:53:02 +0300

    add env

--- src/shared/api/CMC.ts
+++ src/shared/api/CMC.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = "c084c2e0-8b36-4485-878a-0a50a46a11ff";
 
 const coinMarketCapApi = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_DEV ?  "/api" : 'https://pro-api.coinmarketcap.com',
   headers: { "X-CMC_PRO_API_KEY": API_KEY },
 });
 
