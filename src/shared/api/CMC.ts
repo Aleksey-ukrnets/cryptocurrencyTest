f2352345477d0782d7ceb14a5dd6c27602e7d9db
Alexey Ukrainets <aleksey.ukrnets@mail.ru>
2025-02-02 16:00:18 +0300

    add comment

--- src/shared/api/CMC.ts
+++ src/shared/api/CMC.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API_KEY = "c084c2e0-8b36-4485-878a-0a50a46a11ff";
-
+//Можно создавать в целом const с урлами по которым делаем запрос, также ключи все хранить в env
+//Просто написал, чтобы имели ввиду, что я это понимаю, а так как это тестовое, уже не делал это
 const coinMarketCapApi = axios.create({
   baseURL: import.meta.env.VITE_DEV ? "/api" : window.location.origin,
   headers: { "X-CMC_PRO_API_KEY": API_KEY },
